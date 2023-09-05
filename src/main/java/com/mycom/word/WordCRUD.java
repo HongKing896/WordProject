608d22a27dc53f51a2cf772f5291c2c39ca50e67
HongKing896 <butyking7896@gmail.com>
2023-09-05 10:37:59 +0900

    commit through intelliJ for practice

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -20,7 +20,7 @@ public class WordCRUD implements ICRUD{
     @Override
     public Object add() {
         System.out.print(" => 난이도(1,2,3) & 새 단어 입력 :");
-        
+
         return null;
     }
 
